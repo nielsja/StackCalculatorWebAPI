65f22b496568fe2c37fc75e99b0f5aed1e919d81
unknown <NEDL@LI-IST1-0817.doane.local>
2020-06-08 15:44:11 -0500

    First commit

---
+++ Controllers/StackController.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace StackCalculatorWebAPI.Controllers
+{
+    [ApiController]
+    public class StackController : ControllerBase
+    {
+        static Stack<decimal> stack = new Stack<decimal>();
+
+        [HttpGet]
+        [Route("api/Test")]
+        public string Test()
+        {
+            return "Success";
+        }
+
+        [HttpPost]
+        [Route("api/Push")]
+        public void Push([FromBody]decimal number)
+
+        {
+            stack.Push(number);
+        }
+
+        [HttpPost]
+        [Route("api/PushQuery")]
+        public void PushQuery(decimal number)
+        {
+            stack.Push(number);
+        }
+
+        [HttpPost]
+        [Route("api/Print")]
+        public decimal Print()
+        {
+            return stack.Peek();
+        }
+    }
+}
