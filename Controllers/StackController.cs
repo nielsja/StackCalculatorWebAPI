8ea34d804b96ead7e74249deba1854f13bdaf873
unknown <NEDL@LI-IST1-0817.doane.local>
2020-06-08 17:45:25 -0500

    Added calculator API methods

--- Controllers/StackController.cs
+++ Controllers/StackController.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
+using System.Net.Http;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
@@ -21,24 +23,142 @@ namespace StackCalculatorWebAPI.Controllers
 
         [HttpPost]
         [Route("api/Push")]
-        public void Push([FromBody]decimal number)
-
+        public IActionResult Push([FromBody]decimal number)
         {
-            stack.Push(number);
+            if (stack.Count < 10)
+            {
+                stack.Push(number);
+                return Ok();
+            }
+            else
+            {
+                string errorMessage = "Error: Stack is full; could not Push " + number + " on to stack.";
+                return new BadRequestObjectResult(errorMessage);
+            }
         }
 
         [HttpPost]
         [Route("api/PushQuery")]
-        public void PushQuery(decimal number)
+        public IActionResult PushQuery(decimal number)
         {
-            stack.Push(number);
+            if (stack.Count < 10)
+            {
+                stack.Push(number);
+                return Ok();
+            }
+            else
+            {
+                string errorMessage = "Error: Stack is full; could not Push " + number + " on to stack.";
+                return new BadRequestObjectResult(errorMessage);
+            }
         }
 
-        [HttpPost]
-        [Route("api/Print")]
-        public decimal Print()
+        [HttpDelete]
+        [Route("api/Pop")]
+        public IActionResult Pop()
         {
-            return stack.Peek();
+            if (stack.Count == 0)
+            {
+                string errorMessage = "Error: Stack is empty; could not Pop.";
+                return new BadRequestObjectResult(errorMessage);
+            }
+            else
+            {
+                string popped = stack.Pop().ToString();
+                return new OkObjectResult(popped);
+            }
+        }
+
+        [HttpGet]
+        [Route("api/Print")]
+        public IActionResult Print()
+        {
+            if (stack.Count == 0)
+            {
+                string errorMessage = "Error: Stack is empty; nothing to Print.";
+                return new BadRequestObjectResult(errorMessage);
+            }
+            else
+            {
+                string printed = stack.Peek().ToString();
+                return new OkObjectResult(printed);
+            }
+        }
+
+        [HttpGet]
+        [Route("api/Add")]
+        public IActionResult Add()
+        {
+            if (stack.Count < 2)
+            {
+                string errorMessage = "Error: Stack only has " + stack.Count + " items. Could not execute.";
+                return new BadRequestObjectResult(errorMessage);
+            }
+            else
+            {
+                var pop1 = stack.Pop();
+                var pop2 = stack.Pop();
+                var result = pop2 + pop1;
+                stack.Push(result);
+                return Ok();
+            }
+        }
+
+        [HttpGet]
+        [Route("api/Subtract")]
+        public IActionResult Subtract()
+        {
+            if (stack.Count < 2)
+            {
+                string errorMessage = "Error: Stack only has " + stack.Count + " items. Could not execute.";
+                return new BadRequestObjectResult(errorMessage);
+            }
+            else
+            {
+                var pop1 = stack.Pop();
+                var pop2 = stack.Pop();
+                var result = pop2 - pop1;
+                stack.Push(result);
+                return Ok();
+            }
+        }
+
+        [HttpGet]
+        [Route("api/Multiply")]
+        public IActionResult Multiply()
+        {
+            if (stack.Count < 2)
+            {
+                string errorMessage = "Error: Stack only has " + stack.Count + " items. Could not execute.";
+                return new BadRequestObjectResult(errorMessage);
+            }
+            else
+            {
+                var pop1 = stack.Pop();
+                var pop2 = stack.Pop();
+                var result = pop2 * pop1;
+                stack.Push(result);
+                return Ok();
+            }
+        }
+
+        [HttpGet]
+        [Route("api/Divide")]
+        public IActionResult Divide()
+        {
+            if (stack.Count < 2)
+            {
+                string errorMessage = "Error: Stack only has " + stack.Count + " items. Could not execute.";
+                return new BadRequestObjectResult(errorMessage);
+            }
+            else
+            {
+                var pop1 = stack.Pop();
+                var pop2 = stack.Pop();
+                var result = pop2 / pop1;
+                stack.Push(result);
+                return Ok();
+            }
         }
     }
 }
